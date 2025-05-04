1795f90a672b6433ede396c1762abfbdb79406fb
diogo-santos04 <diogo.santos0401@gmail.com>
2025-05-04 11:24:12 -0300

    .

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-
-
 if [ "$APP_ENV" = "production" ]; then
   echo "Executando migrações..."
   php artisan migrate --force
 fi
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 php artisan serve --host=0.0.0.0 --port=8000
 
